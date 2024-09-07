7c9160e2a3f48275891922379fa7353ce3a77377
Shah Faisal <sfaisallive@gmail.com>
2024-09-08 01:56:42 +0500

    Mode user experience hover effects

--- src/components/modes/OfflineMode.js
+++ src/components/modes/OfflineMode.js
@@ -1,13 +1,34 @@
 import React from "react";
-import './OfflineMode.css';
+import "./OfflineMode.css";
 
 const OfflineMode = ({ handleSubModeSelect, handleModeReset }) => {
   return (
     <div className="offline-mode-selection">
       <h2>Play Offline</h2>
-      <button className="mode-control-button" onClick={() => handleSubModeSelect("vsFriends")}>Play vs Friends</button>
-      <button className="mode-control-button" onClick={() => handleSubModeSelect("vsBot")}>Play vs Bot</button>
-      <button className="go-back-button" onClick={handleModeReset}>Go Back</button>
+
+      <div className="button-wrapper">
+        <button
+          className="mode-control-button"
+          onClick={() => handleSubModeSelect("vsFriends")}
+        >
+          Play vs Friends
+        </button>
+        <p className="tooltip-text">Challenge your friends in room!</p> {/* Tooltip */}
+      </div>
+
+      <div className="button-wrapper">
+        <button
+          className="mode-control-button"
+          onClick={() => handleSubModeSelect("vsBot")}
+        >
+          Play vs Bot
+        </button>
+        <p className="tooltip-text">Practice against a bot!</p> {/* Tooltip */}
+      </div>
+
+      <button className="go-back-button" onClick={handleModeReset}>
+        Go Back
+      </button>
     </div>
   );
 };
--- src/components/modes/OnlineMode.js
+++ src/components/modes/OnlineMode.js
@@ -1,13 +1,35 @@
 import React from "react";
-import './OnlineMode.css'
+import "./OnlineMode.css";
 
 const OnlineMode = ({ handleSubModeSelect, handleModeReset }) => {
   return (
     <div className="online-mode-selection">
       <h2>Play Online</h2>
-      <button className="mode-control-button" onClick={() => handleSubModeSelect("createRoom")}>Create Room</button>
-      <button className="mode-control-button" onClick={() => handleSubModeSelect("joinRoom")}>Join Room</button>
-      <button className="go-back-button" onClick={handleModeReset}>Go Back</button>
+
+      <div className="button-wrapper">
+        <button
+          className="mode-control-button"
+          onClick={() => handleSubModeSelect("createRoom")}
+        >
+          Create Room
+        </button>
+        <p className="tooltip-text">Host a room and invite friends!</p>{" "}
+        {/* Tooltip */}
+      </div>
+
+      <div className="button-wrapper">
+        <button
+          className="mode-control-button"
+          onClick={() => handleSubModeSelect("joinRoom")}
+        >
+          Join Room
+        </button>
+        <p className="tooltip-text">Join an existing room!</p> {/* Tooltip */}
+      </div>
+
+      <button className="go-back-button" onClick={handleModeReset}>
+        Go Back
+      </button>
     </div>
   );
 };
